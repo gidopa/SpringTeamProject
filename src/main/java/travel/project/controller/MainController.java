100d4203a5198e3617c8ab274b682093b709af1d
younghoon <bigjodan@gmail.com>
2024-05-01 16:51:04 +0900

    테스트3

--- src/main/java/travel/project/controller/MainController.java
+++ src/main/java/travel/project/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
-//test
+
 @Slf4j
 @Controller
 public class MainController {
@@ -18,7 +18,7 @@ public class MainController {
 		log.info("home controller");
 		return main;
 	}
-	//test
+
 	@GetMapping("/reservation")
 	public String res(Model model){
 		model.addAttribute("center", "../reservation.jsp");
