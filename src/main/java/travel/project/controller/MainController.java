07fb878e706a5bc8c3a14140f7ce003b9d2f6144
younghoon <bigjodan@gmail.com>
2024-05-01 16:45:17 +0900

    장영훈 테스트

--- src/main/java/travel/project/controller/MainController.java
+++ src/main/java/travel/project/controller/MainController.java
@@ -1,11 +1,12 @@
 package travel.project.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+//test
 @Slf4j
 @Controller
 public class MainController {
