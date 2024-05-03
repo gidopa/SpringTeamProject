bdf108ce3054fa5be31511b4cbf8e491f4418dad
gido <jdm2131@naver.com>
2024-05-03 21:11:07 +0900

    feat: Destination 관련

--- src/main/java/travel/project/service/Detination/DestinationService.java
+++ src/main/java/travel/project/service/Detination/DestinationService.java
@@ -1,8 +1,13 @@
 package travel.project.service.Detination;
 
+import travel.project.domain.Attraction;
 import travel.project.domain.Destination;
+
+import java.util.List;
 
 public interface DestinationService {
 
     Destination findDestByName(String destinationName);
+
+    List<Attraction> findAttractionById(long destId);
 }
